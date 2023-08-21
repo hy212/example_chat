5649fefdbe8cfe4c9c21b54d47ffe791afeaa058
huying <xyinghu@tencent.com>
2023-08-21 14:22:38 +0800

    测试

--- functions/postmsg.js
+++ functions/postmsg.js
@@ -4,16 +4,17 @@ addEventListener('fetch', event => {
 const headers = {
     "content-type": "application/json",
 };
+const kvName = 'msgList';
 async function handleRequest(req) {
     const method = req.method;
     console.log("req.method:", method, req);
     try {
         const queryArgs = await req.json();
         //
-        const curMsgs = await kv.get('msgList', "json");
+        const curMsgs = await kv.get(kvName, "json");
         const msgList = curMsgs || [];
         msgList.push(queryArgs);
-        await kv.put('mgsList', msgList);
+        await kv.put(kvName, JSON.stringify(msgList));
         const rspData = {
             code: 1,
             msg: '',
